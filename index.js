7032e6721b3856df080cba6aebfa5c28fdd86c4f
isaac-wahba <isaac.wahba@hotmail.com>
2021-02-18 11:05:09 +0200

    commit project

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const form = document.querySelector("form");
 
-const createLink = () => {};
+
+
+const form = document.querySelector("form");
+const {hash} = window.location;
+
+const message = atob(hash.replace('#',''));
+
+if(message){
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+  document.querySelector('h1').innerHTML = message;
+}
+
 
 const submitForm = (event) => {
   event.preventDefault();
+
+  document.querySelector("#message-form").classList.add('hide');
+  document.querySelector("#link-form").classList.remove('hide');
+
   const input = document.querySelector("#message-input");
-  
+
   const codedValue = btoa(input.value);
-  document.querySelector("#link-input").value = codedValue;
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${codedValue}`;
+  linkInput.select();
 };
 
 form.addEventListener("submit", submitForm);
